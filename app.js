17c510a3bfa3aba25e75942129df523fdbb6103e
Cris Araneta <sircsajerol@gmail.com>
2019-05-18 15:47:47 +0800

    Lacking bootstrap for the home page

--- app.js
+++ app.js
@@ -2,3 +2,21 @@ const express = require('express');
 
 const app = express();
 
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 7000;
+
+const path = require('path');
+
+app.set('views', path.join(__dirname + '/views'));
+app.set('view engine', 'pug');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.get('/', (req,res) =>{
+    res.render('index');
+});
+
+app.listen(port);
+console.log(`Server ready at port ${port}`);
